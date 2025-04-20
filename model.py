25e62a3f20c6f806f473378567d19fbaeb297fbd
dcbothra <102783034+dcbothra@users.noreply.github.com>
2025-04-20 22:59:28 +0530

    Made some changes to model.py

--- model.py
+++ model.py
@@ -5,11 +5,11 @@ class DQN(nn.Module):
     def __init__(self, state_dim, action_dim):
         super(DQN, self).__init__()
         self.net = nn.Sequential(
-            nn.Linear(state_dim, 64),  # Fully connected layer from input to 128 units
-            nn.ReLU(),                 # Activation function
-            nn.Linear(64, 64),       # Hidden layer
+            nn.Linear(state_dim, 64),
             nn.ReLU(),                 
-            nn.Linear(64, action_dim) # Output layer -> Q-values for each action
+            nn.Linear(64, 64),       
+            nn.ReLU(),                 
+            nn.Linear(64, action_dim) 
         )
 
     def forward(self, x):
